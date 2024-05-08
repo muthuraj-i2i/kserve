ecee4c4c48d9fb5a82fcf285a9b1948239f3fcae
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-05-08 19:57:23 +0530

    keda reconciler implementation
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -453,6 +453,7 @@ const (
 const (
 	IstioVirtualServiceKind = "VirtualService"
 	KnativeServiceKind      = "Service"
+	KedaScaledObjectKind    = "ScaledObject"
 )
 
 // GetRawServiceLabel generate native service label
