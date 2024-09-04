ac8c5ee43d96daa6ccbe7294b77d8d876860e02b
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2024-09-04 13:30:43 +0530

    added keda installation in quick install
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- hack/quick_install.sh
+++ hack/quick_install.sh
@@ -22,6 +22,8 @@ export KNATIVE_OPERATOR_VERSION=v1.14.5
 export KNATIVE_SERVING_VERSION=1.13.1
 export KSERVE_VERSION=v0.14.0
 export CERT_MANAGER_VERSION=v1.15.1
+export KEDA_VERSION=2.14.0
+
 SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 export SCRIPT_DIR
 
@@ -41,6 +43,9 @@ uninstall() {
 
    helm uninstall --ignore-not-found cert-manager -n cert-manager
    echo "😀 Successfully uninstalled Cert Manager"
+
+   helm uninstall --ignore-not-found keda -n keda
+   echo "😀 Successfully uninstalled KEDA"
 
    kubectl delete --ignore-not-found=true namespace istio-system
    kubectl delete --ignore-not-found=true namespace cert-manager
@@ -109,6 +114,11 @@ helm install \
    --version ${CERT_MANAGER_VERSION} \
    --set crds.enabled=true
 echo "😀 Successfully installed Cert Manager"
+
+#Install KEDA
+helm repo add kedacore https://kedacore.github.io/charts
+helm install keda kedacore/keda --version ${KEDA_VERSION} --namespace keda --create-namespace --wait
+echo "😀 Successfully installed KEDA"
 
 # Install Knative
 if [ $deploymentMode = "Serverless" ]; then
